1d5e315357f57c627ede396c7e385a1df2d3f16e
Emperor-CJ <2276678161@qq.com>
2020-01-16 20:05:52 +0800

    part_1_8_2suspend与resume方法的缺点_独占

---
+++ src/part001/part1_8暂停线程/part_1_8_2suspend与resume方法的缺点_独占/SynchronizedObject.java
@@ -0,0 +1,7 @@
+package part001.part1_8暂停线程.part_1_8_2suspend与resume方法的缺点_独占;
+
+public class SynchronizedObject {
+    synchronized public void printString(){
+        System.out.println("begin");
+    }
+}
