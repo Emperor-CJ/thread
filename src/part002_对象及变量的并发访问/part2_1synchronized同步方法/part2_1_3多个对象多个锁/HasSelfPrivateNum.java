90036b10b01ade700bb377996e834064fbb68e8c
Emperor-CJ <2276678161@qq.com>
2020-01-20 16:50:44 +0800

    part2_1_3多个对象多个锁

---
+++ src/part002_对象及变量的并发访问/part2_1synchronized同步方法/part2_1_3多个对象多个锁/HasSelfPrivateNum.java
@@ -0,0 +1,26 @@
+package part002_对象及变量的并发访问.part2_1synchronized同步方法.part2_1_3多个对象多个锁;
+
+/**
+ * HasSelfPrivateNum
+ *
+ * @Author: chenjie
+ * @Date: 2020/1/20
+ */
+public class HasSelfPrivateNum {
+    private int num = 0;
+    synchronized public void add(String username){
+        try{
+            if (username.equals("a")){
+                num = 100;
+                System.out.println("a set over");
+                Thread.sleep(2000);
+            }else {
+                num = 200;
+                System.out.println("b set over!");
+            }
+            System.out.println(username + " num=" + num);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
