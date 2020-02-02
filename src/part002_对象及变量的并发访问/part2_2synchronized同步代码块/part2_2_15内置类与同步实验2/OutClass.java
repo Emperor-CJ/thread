376e5bae4deebe98033719ff0f4aa8c371582913
陈杰 <Emperor_CJ@126.com>
2020-02-02 09:56:01 +0800

    part2_2_15内置类与同步实验2

---
+++ src/part002_对象及变量的并发访问/part2_2synchronized同步代码块/part2_2_15内置类与同步实验2/OutClass.java
@@ -0,0 +1,56 @@
+package part002_对象及变量的并发访问.part2_2synchronized同步代码块.part2_2_15内置类与同步实验2;
+
+/**
+ * Created by chenjie on 2020/2/2.
+ */
+public class OutClass {
+    static class InnerClass1 {
+        public void method1(InnerClass2 class2) {
+            String threadName = Thread.currentThread().getName();
+            synchronized (class2) {
+                System.out.println(threadName + " 进入InnerClass1类中的method1方法");
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("i=" + i);
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+
+                    }
+                }
+                System.out.println(threadName + " 离开InnerClass1类中的method1方法");
+            }
+        }
+
+        public synchronized void method2() {
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + " 进入InnerClass1类中的method2方法");
+            for (int i = 0; i < 10; i++) {
+                System.out.println("i=" + i);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+
+                }
+            }
+            System.out.println(threadName + " 离开InnerClass1类中的method2方法");
+
+        }
+    }
+
+    static class InnerClass2 {
+        public synchronized void method1() {
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + " 进入InnerClass2类中的method1方法");
+            for (int i = 0; i < 10; i++) {
+                System.out.println("i=" + i);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+
+                }
+            }
+            System.out.println(threadName + " 离开InnerClass2类中的method1方法");
+
+        }
+    }
+}
